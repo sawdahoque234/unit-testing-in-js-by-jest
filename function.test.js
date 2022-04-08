69eeeaa9fb29ab0fa8ff247acb71ff493066f049
sawdahoque234 <sawdahoque6066@gmail.com>
2022-04-09 01:19:54 +0600

    unit test

--- function.test.js
+++ function.test.js
@@ -2,3 +2,15 @@ const functions=require('./functions')
 test('The Sum is:',()=>{
     expect(functions.add(3,4)).toBe(7)
 })
+test('Should be NUll',()=>{
+    expect(functions.isNUll()).toBeNull()
+})
+test('Should be falsy',()=>{
+    expect(functions.checkValue(0)).toBeFalsy()
+})
+test('My FUll Name :',()=>{
+    expect(functions.createUser()).toEqual({
+        firstName:'sawda',
+        lastName:'hoque',
+    });
+});
--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
 const functions ={
 add:(a,b)=>a+b,
-
+isNUll:()=>null,
+checkValue:x=>x,
+createUser:()=>{
+    const user={firstName:'sawda'}
+    user['lastName']='hoque';
+    return user;
+}
 }
 module.exports=functions
